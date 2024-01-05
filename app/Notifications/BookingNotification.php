10ae3df8370edb589a11f5919dccdcd7e6a2d3a3
MuneebAccrual <muneeb.rehman@accrualhub.com>
2024-01-05 19:12:29 +0500

    Email Testing

--- app/Notifications/BookingNotification.php
+++ app/Notifications/BookingNotification.php
@@ -23,9 +23,10 @@ class BookingNotification extends Notification
      *
      * @return void
      */
-    public function __construct($details)
+    // public function __construct($details)
+    public function __construct()
     {
-        $this->details = $details;
+        // $this->details = $details;
     }
 
     /**
@@ -48,9 +49,10 @@ class BookingNotification extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->greeting(new HtmlString($this->details['greeting']))
-            ->subject($this->details['subject'])
-            ->view('mail.bookingemail',['details'=> $this->details,'url'=>url($this->details['action_url'])]);
+        ->line("This is the test Email");
+            // ->greeting(new HtmlString($this->details['greeting']))
+            // ->subject($this->details['subject'])
+            // ->view('mail.bookingemail',['details'=> $this->details,'url'=>url($this->details['action_url'])]);
             /*->line($this->details['body'])
             ->action($this->details['action_text'], url($this->details['action_url']))
             ->line($this->details['thanks_text']);*/
